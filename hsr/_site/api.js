9213fbe268881fba0f6fa6e49d3b498e6fbddb36
Kevin Xu <kxu144@berkeley.edu>
2023-05-28 02:22:40 -0700

    characters and search bar

---
+++ hsr/_site/api.js
@@ -0,0 +1,43 @@
+// API
+relic_sets = [
+    'band of sizzling thunder',
+    'belobog of the architects',
+    'celestial differentiator',
+    'champion of streetwise boxing',
+    'eagle of twilight line',
+    'firesmith of lavaforging',
+    'fleet of the ageless',
+    'genius of the brilliant stars',
+    'guard of wuthering snow',
+    'hunter of glacial forest',
+    'inert salsotto',
+    'knight of purity palace',
+    'musketeer of wild wheat',
+    'pangalactic commercial enterprise',
+    'passerby of wandering cloud',
+    'space sealing station',
+    'sprightly vonwacq',
+    'talia kingdom of banditry',
+    'thief of shooting meteor',
+    'wastelander of banditry desert',
+];
+relic_types = [
+    'head',
+    'hands',
+    'body',
+    'feet',
+    'planar sphere',
+    'link rope',
+];
+stats = [
+    'hp',
+    'atk',
+    'def',
+    'critRate',
+    'critDMG',
+    'outgoing_healing_boost',
+    'effect_hit_rate',
+    'effect_res',
+    'break_effect',
+    'spd',
+];
